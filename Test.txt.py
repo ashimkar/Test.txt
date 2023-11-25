316bf1ec8cec7889d6711b0ece194fe44480b5c5
ashimkar <150282433+ashimkar@users.noreply.github.com>
2023-11-25 19:48:42 +0600

    Add files via upload

---
+++ Test.txt.py
@@ -0,0 +1,17 @@
+text = "Have a nice day! See ya."
+with open("test.txt", "a") as file:
+    file.write(text)
+import shutil
+shutil.copyfile("test.txt", "copy.txt")
+
+import  os
+source = "test.txt"
+destination = "H:\\Py\\desktop\\test.txt"
+try:
+except
+
+try:
+    with open("Test.txt.py") as file:
+        print(file.read())
+except FileNotFoundError:
+    print("that file was not found")
